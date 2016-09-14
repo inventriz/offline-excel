13a291fbea3994a6f88d569ec36d6886deb033dc
Amit <asingh1@sapient.com>
2016-09-14 23:45:27 +0530

    fixed print header issue.

--- app/gridCtrl.js
+++ app/gridCtrl.js
@@ -9,26 +9,8 @@ app.controller('gridCtrl', function ($scope, $rootScope, $routeParams, $location
         angular.forEach($scope.xd,function(value,index){
             columns.push($scope.xd[index].label);
         });
-
-        $scope.xdColumns = columns;
-        console.log($scope.xdColumns);
+        
     });
-    $scope.xd1 = [
-        {
-            "ID": "1",
-            "primary name": "2",
-            "title": "mytitle1"
-        }, {
-            "ID": "2",
-            "primary": "3",
-            "title": "mytitle2"
-        }, {
-            "ID": "3",
-            "primary": "4",
-            "title": "mytitle3"
-        }
-    ];
- 
-    $scope.xdColumns1 = Object.keys($scope.xd1[0]);  
-    console.log($scope.xdColumns1);
+
+    $scope.xdColumns = columns;
 });
